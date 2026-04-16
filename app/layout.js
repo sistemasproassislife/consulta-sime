67bbafae8a0336f6ebd14d15cfaababa54dcddc1
Sistemas Proassislife <sistemas.proassismed@gmail.com>
2026-04-16 16:27:56 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,11 @@
+export const metadata = {
+  title: "Consulta SIME",
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
